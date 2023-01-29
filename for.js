cccdd519943fba297e7caefa2973b5cd2761bba0
Progga <proggaroy2000@gmail.com>
2023-01-29 18:36:34 +0600

    for loop

---
+++ for.js
@@ -0,0 +1,13 @@
+for(var i=0; i<=10;i++){
+    // console.log(i);
+}
+
+// even number
+for(var i=0; i<=20;i=i+2){
+    console.log(i);
+}
+
+// odd number
+for(var i=1; i<=20;i=i+2){
+    console.log(i);
+}
