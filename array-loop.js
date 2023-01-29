a985c6a7350bee57304b471524fc003999d4913c
Progga <proggaroy2000@gmail.com>
2023-01-29 20:37:20 +0600

    arry-loop

---
+++ array-loop.js
@@ -0,0 +1,13 @@
+var numbers = [23, 43, 54, 76, 58, 98, 71, 31, 53, 77, 88, 99, 100];
+
+for (var i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+    // console.log(number);
+}
+
+var items = ['notebook','mouse','keyboard','pen','bag'];
+for(var i =0; i<items.length; i++){
+    var item = items[i];
+
+    console.log(item);
+}
---
+++ break-contineu.js
@@ -0,0 +1,11 @@
+var numbers = [90,34,67,55,12,47,87,67,64,33,76];
+
+for( var i=0; i<numbers.length;i++){
+    var number = numbers[i];
+    if(number > 67)
+     break;
+    
+   
+    console.log(number);
+}
+
